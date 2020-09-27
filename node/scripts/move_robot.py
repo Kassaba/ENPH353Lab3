3cb7838463134098c2b05dd689221ffd7d66176e
Andrew Kassab <andrewkassab17@gmail.com>
2020-09-26 18:46:55 -0700

    Working line following

--- node/scripts/move_robot.py
+++ node/scripts/move_robot.py
@@ -5,7 +5,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import numpy
-
+import time
 
 def callback(data):
   try:
@@ -14,15 +14,32 @@ def callback(data):
     print(e)
   img=numpy.asarray(cv_image)
 
-  move.linear.x = 0.5
-  move.angular.z = 0.5
-  pub.publish(move)
-  print("Test")
+  threshold = 150
+  # cv2.destroyAllWindows()
+  # cv2.imshow('img', img)
+  # cv2.waitKey(0)
+  sum = 0
+  count = 0
+  previous_angular = 0
+  for i in range(len(img[750,:])):
+    if img[750,i] < threshold:
+      sum += i
+      count += 1
+
+  if count == 0:
+    move.linear.x = 0
+    move.angular.z = previous_angular
+  else:
+    move.linear.x = 0.3
+    previous_angular = -0.01 * (sum / count - 400)
+    move.angular.z = previous_angular
+    pub.publish(move)
 
 
 rospy.init_node('move_robot')
 rospy.Subscriber("/robot/camera/image_raw", Image, callback)
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 move = Twist()
+move.linear.x = 0.3
 bridge = CvBridge()
 rospy.spin()
